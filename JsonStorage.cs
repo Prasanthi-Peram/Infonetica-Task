6a7ebe84f6bcf259a18104af5647b74c9b187347
Prasanthi-Peram <prasanthiperam29@gmail.com>
2025-07-18 02:30:57 +0530

    feat: Implement persistence of workflow instances

--- JsonStorage.cs
+++ JsonStorage.cs
@@ -4,21 +4,63 @@ using StateMachine.Models;
 public static class JsonStorage
 
 {
-    private static readonly string FilePath = "workflows.json";
+    private static readonly string WorkflowFilePath = "workflows.json";
+    private static readonly string WorkflowInstancesFilePath = "workflow-instances.json";
 
-     public static Dictionary<string, Workflow> Load()
+     // Workflows
+    public static Dictionary<string, Workflow> LoadWorkflows()
     {
-        if (!File.Exists(FilePath))
+        if (!File.Exists(WorkflowFilePath))
             return new Dictionary<string, Workflow>();
-        var json = File.ReadAllText(FilePath);
 
-         return JsonSerializer.Deserialize<Dictionary<string, Workflow>>(json)
-               ?? new Dictionary<string, Workflow>();
+
+        var json = File.ReadAllText(WorkflowFilePath);
+        return JsonSerializer.Deserialize<Dictionary<string, Workflow>>(json, SerializerOptions())
     }
 
-   public static void Save(Dictionary<string, Workflow> store)
+   public static void SaveWorkflows(Dictionary<string, Workflow> store)
     {
-         var json = JsonSerializer.Serialize(store, new JsonSerializerOptions { WriteIndented = true });
-        File.WriteAllText(FilePath, json);
+          var json = JsonSerializer.Serialize(store, SerializerOptions(true));
+
+
+
+        File.WriteAllText(WorkflowFilePath, json);
+
+
+    }
+
+    // Workflow Instances
+
+
+    public static Dictionary<string, WorkflowInstance> LoadWorkflowInstances()
+
+
+    {
+
+        if (!File.Exists(WorkflowInstancesFilePath))
+            return new Dictionary<string, WorkflowInstance>();
+
+        var json = File.ReadAllText(WorkflowInstancesFilePath);
+        return JsonSerializer.Deserialize<Dictionary<string, WorkflowInstance>>(json, SerializerOptions())
+               ?? new Dictionary<string, WorkflowInstance>();
+    }
+
+
+    public static void SaveWorkflowInstances(Dictionary<string, WorkflowInstance> instances)
+    {
+        var json = JsonSerializer.Serialize(instances, SerializerOptions(true));
+        File.WriteAllText(WorkflowInstancesFilePath, json);
+    }
+
+
+    // Serializer Settings
+    private static JsonSerializerOptions SerializerOptions(bool indent = false)
+
+    {
+        return new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            WriteIndented = indent
+        };
     }
 }
